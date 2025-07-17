30e87376c441d8e6d6ef54d5d8c8ea7238aac669
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-07-17 13:16:49 +0530

    Make 404 Pages and all pages design and improvement

--- htmlrender.js
+++ htmlrender.js
@@ -1,22 +1,32 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+const abspath = path.resolve('view')
 const app = express();
 
 app.get('/', (req, res) => {
-    const abspath = path.resolve('view/home.html')
-    res.sendFile(abspath)
+    res.sendFile(abspath + "/home.html")
     })
 
 app.get('/login', (req, res) => {
-    const abspath = path.resolve('view/login.html')
-    res.sendFile(abspath)
+    res.sendFile(abspath + "/login.html")
+    })
+
+app.get('/submit', (req, res) => {
+    res.sendFile(abspath + "/submit.html")
     }) 
 
 app.get('/about', (req, res) => {
-    const abspath = path.resolve('view/about.html')
-    res.sendFile(abspath)
+    res.sendFile(abspath + "/about.html")
     })
+
+app.get('/contact', (req, res) => {
+    res.sendFile(abspath + "/contact.html")
+    })
+
+app.use((req, res) => {
+    res.status(404).sendFile(abspath + "/4O4Page.html");
+});
+
   
   app.listen(3200);
