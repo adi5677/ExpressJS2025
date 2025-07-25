3db7714015e0a25d2d5c28cebbf7867976934e90
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-07-25 15:55:39 +0530

    Implement API endpoint for student data retrieval and fix variable naming in students.ejs

--- mongoin.js
+++ mongoin.js
@@ -1,10 +1,33 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 
+const app = express();
+
+app.set("view engine", "ejs");
+
 const dbName = "college"
 const url = "mongodb://localhost:27017"
 
 const client = new MongoClient(url);
+
+client.connect().then((connection)=>{
+
+    const db = connection.db(dbName);
+
+    app.get("/api", async (req, res) => {
+        const collection = db.collection("students");   
+        const students = await collection.find().toArray();
+        res.json(students);
+        console.log(students);
+    })
+
+    app.get("/ui", async (req, res) => {
+        const collection = db.collection("students");   
+        const students = await collection.find().toArray();
+        res.render('students', {students: students})
+    });
+
+});
 
 // async function dbCOnnection() {
 
@@ -19,23 +42,21 @@ const client = new MongoClient(url);
 
 // dbConnection();
 
-const app = express();
 
-app.set("view engine", "ejs");
 
-app.listen(3200)
+// app.listen(3200)
 
-app.get("/", async (req, res) => {
+// app.get("/", async (req, res) => {
 
-    await client.connect()
-    const db = client.db(dbName);
-    const collection = db.collection('students')
+//     await client.connect()
+//     const db = client.db(dbName);
+//     const collection = db.collection('students')
 
-    const students = await collection.find().toArray();
-    console.log(students);
+//     const students = await collection.find().toArray();
+//     console.log(students);
 
-    // res.send("Data will be hare");
-    res.render('students', {students: students})
-  })
+//     // res.send("Data will be hare");
+//     res.render('students', {students: students})
+//   })
 
 app.listen(3200);
