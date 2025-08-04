c82a0b0fdbc349bc9a7edbf7e37fbdc26e56d03f
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-04 15:35:43 +0530

    Implement Express server with MongoDB connection and student management routes

---
+++ postMethodMongo.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import express from "express";
+// import studentModel from "./model/studentModel.js";
+
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/college')
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+app.get('/students', async (req, res) => {
+  
+        const studentData = await studentModel.find()
+        res.send(studentData);
+    
+});
+
+app.post('/save', async (req, res) => {
+    console.log(req.body);
+    const studentData = new studentModel({
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+    });
+
+    await studentData.save();
+
+    
+    res.send({
+
+        message: "Data saved successfully",
+        success: true,
+        storedInfo: studentData
+
+    })
+});
+
+app.listen(3200);
+
+
